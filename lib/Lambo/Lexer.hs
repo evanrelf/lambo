883992b22401d279ff2630028204dcb817ce15ad
Evan Relf <evan@evanrelf.com>
2021-07-15 22:08:25 -0700

    Add `Token` type

--- lib/Lambo/Lexer.hs
+++ lib/Lambo/Lexer.hs
@@ -1,4 +1,19 @@
 module Lambo.Lexer
-  (
+  ( Token (..)
   )
 where
+
+import Data.Text (Text)
+
+
+data Token
+  = Token_Lambda
+    -- ^ \
+  | Token_Dot
+    -- ^ .
+  | Token_Variable Text
+    -- ^ x
+  | Token_OpenParen
+    -- ^ (
+  | Token_CloseParen
+    -- ^ )
