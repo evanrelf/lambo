8793538884f8cb263ef89a3cdf0dcaa1c6373f02
Evan Relf <evan@evanrelf.com>
2021-07-15 22:16:56 -0700

    Add `Expression` type

--- lib/Lambo/Syntax.hs
+++ lib/Lambo/Syntax.hs
@@ -1,4 +1,16 @@
 module Lambo.Syntax
-  (
+  ( Expression (..)
   )
 where
+
+import Data.Text (Text)
+
+
+-- | \f. (\x. f (x x)) (\x. f (x x))
+data Expression
+  = Expression_Variable Text
+    -- ^ x
+  | Expression_Abstraction Text Expression
+    -- ^ \ <variable> . <expression>
+  | Expression_Application Expression Expression
+    -- ^ ( <expression> <expression> )
