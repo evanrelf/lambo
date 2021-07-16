6500251a1825c69546e5c0a69896592c3f5407ca
Evan Relf <evan@evanrelf.com>
2021-07-15 23:09:44 -0700

    Write lexer

--- lib/Lambo/Lexer.hs
+++ lib/Lambo/Lexer.hs
@@ -1,15 +1,24 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lambo.Lexer
   ( Token (..)
   , lex
   )
 where
 
+import Control.Applicative (empty)
+import Data.Foldable (asum)
 import Data.Text (Text)
 import Data.Void (Void)
 import Prelude hiding (lex)
 
+import qualified Data.Char as Char
 import qualified Data.Text as Text
 import qualified Text.Megaparsec as Megaparsec
+import qualified Text.Megaparsec.Char as Megaparsec
+import qualified Text.Megaparsec.Char.Lexer as Megaparsec.Lexer
 
 
 data Token
@@ -23,6 +32,7 @@ data Token
     -- ^ (
   | Token_CloseParen
     -- ^ )
+  deriving stock Show
 
 
 type Parser = Megaparsec.Parsec Void Text
@@ -38,4 +48,51 @@ lex source =
 
 
 parseTokens :: Parser [Token]
-parseTokens = undefined
+parseTokens = parseSpace *> Megaparsec.manyTill parseToken Megaparsec.eof
+
+
+parseToken :: Parser Token
+parseToken = asum
+  [ Token_Lambda
+      <$ parseSymbol "\\"
+  , Token_Dot
+      <$ parseSymbol "."
+  , Token_Variable
+      <$> parseVariable
+  , Token_OpenParen
+      <$ parseSymbol "("
+  , Token_CloseParen
+      <$ parseSymbol ")"
+  ]
+
+
+parseVariable :: Parser Text
+parseVariable = parseLexeme do
+  c <- Megaparsec.satisfy \char -> any ($ char)
+    [ Char.isAsciiLower
+    , (==) '_'
+    ]
+  cs <- Megaparsec.takeWhileP Nothing \char -> any ($ char)
+    [ Char.isAsciiLower
+    , Char.isAsciiUpper
+    , Char.isDigit
+    , (==) '_'
+    , (==) '\''
+    ]
+  pure (c `Text.cons` cs)
+
+
+parseLexeme :: Parser a -> Parser a
+parseLexeme = Megaparsec.Lexer.lexeme parseSpace
+
+
+parseSymbol :: Text -> Parser Text
+parseSymbol = Megaparsec.Lexer.symbol parseSpace
+
+
+parseSpace :: Parser ()
+parseSpace =
+  Megaparsec.Lexer.space
+    Megaparsec.space1
+    (Megaparsec.Lexer.skipLineComment "--")
+    empty
