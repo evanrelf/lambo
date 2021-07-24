c6273f3d0e3c803e511c59977a05af33ac725512
Evan Relf <evan@evanrelf.com>
2021-07-24 10:23:00 -0700

    Start working on evaluator

--- lib/Lambo.hs
+++ lib/Lambo.hs
@@ -3,6 +3,7 @@ module Lambo
   )
 where
 
+import Lambo.Evaluator as X
 import Lambo.Expression as X
 import Lambo.Lexer as X
 import Lambo.Parser as X
---
+++ lib/Lambo/Evaluator.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Lambo.Evaluator
+  ( evaluate
+  )
+where
+
+import Lambo.Expression (Expression (..))
+
+
+evaluate :: Expression -> Expression
+evaluate = \case
+  Expression_Variable name index ->
+    undefined
+
+  Expression_Abstraction argument definition ->
+    undefined
+
+  Expression_Application function argument ->
+    undefined
